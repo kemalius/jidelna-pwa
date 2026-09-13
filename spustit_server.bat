efbe78fff1af7583943d7f2445db5f61d7d443c2
kemalius <152340375+kemalius@users.noreply.github.com>
2026-09-13 22:48:56 +0200

    Add files via upload

---
+++ spustit_server.bat
@@ -0,0 +1,9 @@
+@echo off
+title Spousteni serveru Jidelna PWA
+echo ===========================================
+echo   Spoustim backend server Jidelna PWA...
+echo ===========================================
+cd /d "%~dp0backend"
+python -m pip install -r requirements.txt
+python -m uvicorn main:app --host 127.0.0.1 --port 8000 --reload
+pause
